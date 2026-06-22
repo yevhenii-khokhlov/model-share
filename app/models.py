a6e2e5b34f7fbd420e91a0054511e89fb4654c8a
Yevhenii Khokhlov <74196502+yevhenii-khokhlov@users.noreply.github.com>
2026-06-22 17:16:49 +0300

    Fix: Use pbkdf2:sha256 for password hashing on macOS

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     models = db.relationship("Model", backref="author", lazy=True, cascade="all, delete-orphan")
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
